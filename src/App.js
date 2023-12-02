b22670844534fb728d62baef6e421c9d2595e52a
seif1000 <seifwin74@gmail.com>
2023-12-03 00:24:17 +0100

    update bucket name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hellooo</p>
+        <p>hellooooo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
